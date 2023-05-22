8f01cbbb8abd1bfbbe90cec6f01990d6ed19dacb
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-22 19:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const time = document.getElementById("timer")
+const d = new Date();
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+function checkAmPm(hour){
+    if(hour >= 0 && hour < 12){
+        return "AM"
+    }
+    return "PM"
+}
+function startTime() {
+    const today = new Date();
+    let hour = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    
+    // add a zero in front of numbers<10
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    time.innerHTML = `${d.toLocaleDateString()}, ${hour}:${minutes}:${seconds} ${checkAmPm(hour)}`;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+startTime();
